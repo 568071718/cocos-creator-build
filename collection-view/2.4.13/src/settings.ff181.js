5e9594c3ba63de25bb062162eecad8543c7b616b
o.o.c <ooc@oocdeMacBook-Pro.local>
2024-05-12 23:52:27 +0800

    update

---
+++ collection-view/2.4.13/src/settings.ff181.js
@@ -0,0 +1,23 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/home/home.fire",
+    orientation: "portrait",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "cb4f5",
+        main: "52ca3"
+    }
+};
